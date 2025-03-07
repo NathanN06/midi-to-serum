a7c19b79129f4b21f64b0c3636b424676a934a2f
NathanN06 <nathanhienguyen@gmail.com>
2025-03-07 15:23:21 +0700

    Delete Tests directory

--- Tests/test_midi_parser.py
+++
@@ -1,8 +0,0 @@
-from backend.midi_parser import parse_midi
-
-def test_parse_midi():
-    midi_file = "test_files/sample.mid"  # Use a sample MIDI file
-    midi_data = parse_midi(midi_file)
-    assert "tempo" in midi_data
-    assert "notes" in midi_data
-    assert len(midi_data["notes"]) > 0
